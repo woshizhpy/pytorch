43cff8f3d8669c9da0910808b25db16caa67cb1b
albanD <desmaison.alban@gmail.com>
2019-10-17 16:22:24 -0700

    add pybind version of HANDLE_TH_ERRORS
    ghstack-source-id: 55597ba06a6834cab0127ca9a180d55de19b6b62
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/26614

--- test/test_torch.py
+++ test/test_torch.py
@@ -19,6 +19,7 @@ import textwrap
 import zipfile
 from torch._utils_internal import get_file_path_2
 from torch.utils.dlpack import from_dlpack, to_dlpack
+from torch.utils import cpp_extension
 from torch._utils import _rebuild_tensor
 from torch._six import inf, nan, string_classes, istuple
 from itertools import product, combinations, combinations_with_replacement, permutations
@@ -5686,6 +5687,94 @@ tensor([[[1., 1., 1.,  ..., 1., 1., 1.],
         e1.fill_diagonal_(v, wrap=True)
         self.assertEqual(e1, e2)
 
+    def test_warning_catch(self):
+        source = '''
+        // error_type:
+        // 0: no error
+        // 1: torch::TypeError
+        // 2: python_error()
+        // 3: py::error_already_set
+        at::Tensor foo(at::Tensor x, int error_type) {
+            std::ostringstream err_stream;
+            err_stream << "Error with "  << x.type();
+
+            TORCH_WARN(err_stream.str());
+            if(error_type == 1) {
+                throw torch::TypeError(err_stream.str().c_str());
+            }
+            if(error_type == 2) {
+                PyObject* obj = PyTuple_New(-1);
+                TORCH_CHECK(!obj);
+                // Pretend it was caught in a different thread and restored here
+                auto e = python_error();
+                e.persist();
+                e.restore();
+                throw e;
+            }
+            if(error_type == 3) {
+                throw py::key_error(err_stream.str());
+            }
+            return x.cos();
+        }
+        '''
+
+        # Ensure double type for hard-coded c name below
+        t = torch.rand(2).double()
+        cpp_tensor_name = r"Variable\[CPUDoubleType\]"
+
+        # Without error handling, the warnings cannot be catched
+        # and the Tensor type names are not cleaned
+        warn_mod = cpp_extension.load_inline(name='warn_mod',
+                                             cpp_sources=[source],
+                                             functions=['foo'],
+                                             with_pytorch_error_handling=False)
+
+        with warnings.catch_warnings(record=True) as w:
+            warn_mod.foo(t, 0)
+            self.assertEqual(len(w), 0)
+
+            # pybind translate all our errors to RuntimeError
+            with self.assertRaisesRegex(RuntimeError, cpp_tensor_name):
+                warn_mod.foo(t, 1)
+            self.assertEqual(len(w), 0)
+
+            # pybind does not understand our "python_error"
+            with self.assertRaisesRegex(RuntimeError, "std::exception"):
+                warn_mod.foo(t, 2)
+            self.assertEqual(len(w), 0)
+
+            with self.assertRaisesRegex(KeyError, cpp_tensor_name):
+                warn_mod.foo(t, 3)
+            self.assertEqual(len(w), 0)
+
+
+        warn_mod = cpp_extension.load_inline(name='warn_mod',
+                                             cpp_sources=[source],
+                                             functions=['foo'],
+                                             with_pytorch_error_handling=True)
+
+
+        with warnings.catch_warnings(record=True) as w:
+            # Catched with no error should be detected
+            warn_mod.foo(t, 0)
+            self.assertEqual(len(w), 1)
+
+            # Catched with cpp error should not be detected
+            with self.assertRaisesRegex(TypeError, t.type()):
+                warn_mod.foo(t, 1)
+            self.assertEqual(len(w), 1)
+
+            # Catched with python error should not be detected
+            with self.assertRaisesRegex(SystemError, "bad argument to internal function"):
+                warn_mod.foo(t, 2)
+            self.assertEqual(len(w), 1)
+
+            # Catched with pybind error should not be detected
+            # Note that there is no type name translation for pybind errors
+            with self.assertRaisesRegex(KeyError, cpp_tensor_name):
+                warn_mod.foo(t, 3)
+            self.assertEqual(len(w), 1)
+
 # Functions to test negative dimension wrapping
 METHOD = 1
 INPLACE_METHOD = 2
@@ -5733,7 +5822,6 @@ def make_neg_dim_test(name, tensor_arg, arg_constr, types, extra_dim=0):
                 self.assertEqual(a, b)
 
     return neg_dim_test
-
 
 def idx_tensor(size, max_val):
     return torch.LongTensor(*size).random_(0, max_val - 1)
@@ -6482,6 +6570,12 @@ class TestTorchDeviceType(TestCase):
         # Batch matrices
         M = torch.randn(3, 3, 3, dtype=dtype, device=device)
         run_test(M)
+
+        # one batch
+        matrix = random_fullrank_matrix_distinct_singular_value(5, 1).to(device)
+        matrix_inverse = torch.inverse(matrix)
+        expected_inv = matrix.squeeze(0).inverse()
+        self.assertEqual(matrix_inverse, expected_inv.unsqueeze(0))
 
         # Many batch matrices
         M = torch.randn(2, 3, 3, 3, dtype=dtype, device=device)
@@ -13982,7 +14076,6 @@ def generate_tensor_op_tests(cls):
 
 class TestTensorDeviceOps(TestCase):
     pass
-
 
 class TestTorch(TestCase, _TestTorchMixin):
     pass
